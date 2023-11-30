54da8c9a0a5cc56c532566280608f684fcdc83cf
aleckrsek <krseka@oregonstate.edu>
2023-11-30 13:46:38 -0800

    Add more javascript and classes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,38 @@
+var infoBox;
+var gridBox;
+var contentBox;
+var content;
+var starterBox;
+var homeNav;
+var infoNav;
+
 function init(){
-  var infoBox = document.getElementById("infoBox");
-  infoBox.style.display="none";
+  infoBox = document.getElementById("infoBox");
+  gridBox = document.getElementById("gridBox");
+  contentBox = document.getElementById("contentBox");
+  content = document.getElementById("content");
+  starterBox = document.getElementById("mainBox");
+  homeNav = document.getElementById("homeNav");
+  infoNav = document.getElementById("infoNav");
+  showHome();
 }
 
 window.onload = init;
 
 function showInfo(){
-  var starterBox = document.getElementById("mainBox")
   starterBox.style.display="none";
   infoBox.style.display="block";
+  contentBox.style.display="block";
+  content.style.display="block";
+  homeNav.classList.remove("active");
+  infoNav.classList.add("active");
+}
+
+function showHome(){
+  infoBox.style.display="none";
+  contentBox.style.display="none";
+  content.style.display="none";
+  starterBox.style.display="block";
+  homeNav.classList.add("active");
+  infoNav.classList.remove("active");
 }
